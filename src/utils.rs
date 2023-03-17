11aa283fd4b819c095269ee5fa8e7d54428ae5fa
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-03-17 18:58:14 +0100

    Update fix_annotation_text

--- src/utils.rs
+++ src/utils.rs
@@ -25,7 +25,10 @@ pub fn parse_lang(s: &str) -> String {
 }
 
 pub fn fix_annotation_text(text: &str) -> String {
-    let mut temp_text = text.replace("<br>", "\n").replace("\\n", "\n");
+    let mut temp_text = text
+        .replace("<br>", "\n")
+        .replace("\\n", "\n")
+        .replace("\\\"", "\"");
 
     while temp_text.contains("  ") {
         temp_text = temp_text.replace("  ", " ");
@@ -44,7 +47,37 @@ mod tests {
     use crate::utils::fix_annotation_text;
 
     #[test]
-    fn test_fix_annotation_text() {
+    fn test_fix_annnotation_text_remove_extra_spaces() {
+        let input = "    ";
+        let expected_result = " ";
+
+        let result = fix_annotation_text(input);
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn test_fix_annotation_text_replace_br() {
+        let input = "a<br>b";
+        let expected_result = "a\nb";
+
+        let result = fix_annotation_text(input);
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn test_fix_annotation_text_extra_slashes() {
+        let input = "a \\n b \\\"";
+        let expected_result = "a \n b \"";
+
+        let result = fix_annotation_text(input);
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn test_fix_annotation_text_large() {
         let input = "\n    <p class=book>Этот роман уже стал культовым.\n    <p class=book>Это — одна из самых читаемых книг русскоязычного Интернета, по количеству скачивании соперничающая с «Метро 2033» Глуховского и «Мародером» Беркема аль Атоми.\n    <p class=book>Это — лучшая антиутопия о надвигающейся гражданской войне.\n    <p class=book>Ближайшее будущее. Русофобская политика «оранжевых» разрывает Украину надвое. «Западенцы» при поддержке НАТО пытаются силой усмирить Левобережье. Восточная Малороссия отвечает оккупантам партизанской войной. Наступает беспощадная «эпоха мертворожденных»…\n   ";
         let expected_result = "\n Этот роман уже стал культовым.\n Это — одна из самых читаемых книг русскоязычного Интернета, по количеству скачивании соперничающая с «Метро 2033» Глуховского и «Мародером» Беркема аль Атоми.\n Это — лучшая антиутопия о надвигающейся гражданской войне.\n Ближайшее будущее. Русофобская политика «оранжевых» разрывает Украину надвое. «Западенцы» при поддержке НАТО пытаются силой усмирить Левобережье. Восточная Малороссия отвечает оккупантам партизанской войной. Наступает беспощадная «эпоха мертворожденных»…\n ";
 
