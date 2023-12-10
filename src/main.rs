88d62f5c76426c3586332318bb57ffec81eb9571
Bulat Kurbanov <kurbanovbul@gmail.com>
2023-12-10 22:57:49 +0100

    Update deps

--- src/main.rs
+++ src/main.rs
@@ -47,10 +47,8 @@ async fn start_app() {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     log::info!("Start webserver...");
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
     log::info!("Webserver shutdown...")
 }
 
